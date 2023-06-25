39509f2aded6b51e4f11720f44b2165179a71643
Katy_Zhe <nekhaeva.e@yandex.ru>
2023-06-25 15:49:25 +0300

    cors

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(requestLogger);
-app.use(cors);
+app.use(cors());
 
 app.get('/crash-test', () => {
   setTimeout(() => {
