95ebbafebe806598735f674b087b8b95feed745e
Katy_Zhe <nekhaeva.e@yandex.ru>
2023-06-28 07:24:23 +0300

    cors settings update

--- app.js
+++ app.js
@@ -1,34 +1,43 @@
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
-const { errors } = require('celebrate');
-const helmet = require('helmet');
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const { errors } = require("celebrate");
+const helmet = require("helmet");
 
-const cors = require('cors');
+const cors = require("cors");
 const router = require("./routes/index");
-const errorHandler = require('./middlewares/errorHandler');
-const { requestLogger, errorLogger } = require('./middlewares/logger');
+const errorHandler = require("./middlewares/errorHandler");
+const { requestLogger, errorLogger } = require("./middlewares/logger");
 
 const { PORT = 3000 } = process.env;
 const app = express();
-const MONGO_URL_DEV = 'mongodb://127.0.0.1:27017/bitfilmsdb';
+const MONGO_URL_DEV = "mongodb://127.0.0.1:27017/bitfilmsdb";
 const { NODE_ENV, MONGO_URL } = process.env;
 
-mongoose.connect(NODE_ENV === 'production' ? MONGO_URL : MONGO_URL_DEV, {
+mongoose.connect(NODE_ENV === "production" ? MONGO_URL : MONGO_URL_DEV, {
   family: 4,
 });
 
 app.use(helmet());
-app.use(cors({credentials: true, origin: 'http://katyzhe.nomoredomains.rocks'}));
+app.use(
+  cors({
+    credentials: true,
+    origin: [
+      "https://katyzhe.nomoredomains.rocks",
+      "http://katyzhe.nomoredomains.rocks",
+    ],
+    default: "https://katyzhe.nomoredomains.rocks",
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(requestLogger);
 
-app.get('/crash-test', () => {
+app.get("/crash-test", () => {
   setTimeout(() => {
-    throw new Error('Сервер сейчас упадёт');
+    throw new Error("Сервер сейчас упадёт");
   }, 0);
-}); 
+});
 
 app.use(router);
 
